4af3e1bb0800e86e9e548fab93e295b6700a3506
Jacob Werbin <jwerbin95@gmail.com>
2018-12-14 17:57:45 -0600

    Section 3: H Whats in your closet done

--- app.js
+++ app.js
@@ -95,4 +95,42 @@ console.log("Hello, my name is " + myName)
 
   number<5 ? console.log("little number") : number>10 ? console.log("big number") : console.log("monkey")
 
-  
+  const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+console.log("Kristn is rocking that " + kristynsCloset[2] + " today!")
+kristynsCloset.splice(6, 0, "raybans")
+kristynsCloset[5] = "stained knit hat"
+let thomsShirt = thomsCloset[0][0]
+let thomsPants = thomsCloset[1][0]
+let thomsAccessory = thomsCloset[2][0]
+console.log("Thoms is wearing "+thomsShirt+" "+thomsPants+" "+thomsAccessory+" today!")
+thomsCloset[1][2] = "Footie Pajamas"
