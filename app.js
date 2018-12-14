f7f309f0dbfbd3467f66f2c8a859f0d7b3b5c5e7
Julius Camarillo <julcamarillo17@gmail.com>
2018-12-14 16:16:54 -0600

    #2 done

---
+++ app.js
@@ -0,0 +1,9 @@
+//2.
+let firstVariable = "Hello World"
+firstVariable = 6
+console.log(firstVariable)
+let secondVariable = firstVariable
+secondVariable = "yooo"
+console.log(firstVariable)
+let yourName = "Jules Camarillo"
+console.log("Hello, my name is " + yourName +".")
