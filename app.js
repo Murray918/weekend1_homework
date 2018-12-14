c43cf373acf5103960877611703b0dabb0ee40df
ohheyitslance <ohheyitslance@ohheyitslances-MacBook.local>
2018-12-14 16:41:31 -0600

    completed Q+A

---
+++ app.js
@@ -0,0 +1,8 @@
+// I. Variables & Datatypes
+//	 A. Q+A
+// 		1. After declaring the variable assign a value with the equals sign (let x = 0)
+// 		2. Change the value of a variable after declaring it by calling it and reassigning the value with the equals sign (x = 3)
+// 		3. Use the equals sign to assign the value of a existing variable to a new variable (let y = x)
+//  	4. You DECLARE the existence of a variable. You ASSIGN the value of the variable after declaring it. You DEFINE a function by giving it properties.
+// 		5. You use pseudocoding to talk out coding prior to actually writing code. You pseudocode to talk out some of the 
+// 		6. 75-90% planning, and 10-25% writing code
