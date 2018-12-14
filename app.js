9f288e11dcdc868f81f9b5bde6772fcfdc72739b
pittpir <pittpir@gmail.com>
2018-12-14 17:02:19 -0600

    Part 1 -- Q & A Answers

---
+++ app.js
@@ -0,0 +1,30 @@
+//1) Variables & Datatypes
+
+/*
+Q.1 How do we assign a value to a variable?
+A.1 Use the keywords let, const or var to assign a variable.  For example...
+let i = 0;
+
+Q.2 How do we change the value of a variable?
+A.2 use a single "=" equal sign to change the value of a variable.  For example...
+i=5;
+
+
+Q.3 How do we assign an existing variable to a new variable?
+let x = i;
+
+
+Q.4 Remind me, what are declare, assign, and define?
+A.4 Declare  this is the let i part
+Assign this is the "=" part
+Define this is the value.  
+
+Q.5 What is pseudocoding and why should you do it?
+A.5 It gives high level flow of the program and is human readable vs writing actual code.  It will contain loops showing the flow and steps to complete the program 
+
+Q.6 What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+A.6  Thinking about how to solve the problem should take ~80% of the time and coding should take ~20%.
+     Another average is a programmer can program 10 lines of tested code per day!
+
+
+*/
