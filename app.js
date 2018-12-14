30d59a88af8f00faeda6b14777aaf1ecf63e9e74
Jacob Werbin <jwerbin95@gmail.com>
2018-12-14 16:00:42 -0600

    first set of questions answered

---
+++ app.js
@@ -0,0 +1,7 @@
+//You assign values to variables using the equals sign(let var = 12)
+//You change the value of a variable using the equals sign after the varioable has been declared(var=12)
+//You use the equals sign to assign a new variable to an existing variable (let newVar = oldvar)
+//declaring a variable is allocating space in memory for a new variable using the let, var or const keywords. Assigning a variable is giving it a value using the equals sign. Defining a variable is the same as declaring a variable
+//Pseudocoding is creating a code/english hybid algorithm to wrap your head around the problem before you start on it so the code is easier to implement
+//60%
+
