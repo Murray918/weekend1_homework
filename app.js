9ccec66d122e7a6b3fc191f1602bd468dd755643
Andrew Aluko <aluko17@Andrews-MacBook-Air.local>
2018-12-14 16:40:06 -0600

    Q + A and Strings

---
+++ app.js
@@ -0,0 +1,36 @@
+/* A.
+let create space 
+let = new value
+x = y
+
+
+Pseudocode is writing out your code in a simplified manner
+
+75% thinking 25% typing code
+*/
+//1.
+let firstVariable = "Hello World"
+firstVariable = 7
+let secondVariable = firstVariable
+secondVariable = "Hello"
+console.log(firstVariable)
+
+// firstVariable = 7
+
+let yourName = "Andrew"
+
+let name = "hello, my name is " + yourName
+console.log(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
