0331b94bbbabb13ebc55557accfead0f05d9bf02
AJ <ahumphries102@gmail.com>
2018-12-14 16:11:11 -0600

    Completed section 1

---
+++ app.js
@@ -0,0 +1,18 @@
+// How do we assign a value to a variable?
+// 	let x = 0 
+
+// How do we change the value of a variable?
+//	numbers can be incremented or decremented for example. Strings can be changed by assigning a new string to the same variable.
+
+// How do we assign an existing variable to a new variable?
+// 	let x = 0 and then we can say let y = x
+
+// Remind me, what are declare, assign, and define?
+// 	Declare would be creating a variable like, let x. We then assign a value to x such 0 which looks like let x = 0.
+
+// What is pseudocoding and why should you do it?
+// 	pseudocoding is a term used to describe solving technical problems in the real world. It helps by giving a better understanding of solving problems.
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// 	How ever long it takes to understand the problem.
+
