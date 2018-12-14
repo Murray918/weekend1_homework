8563174e0c897db807c7bacc75b72de99fe79adf
Johnathan <reddjohnathan@gmail.com>
2018-12-14 16:13:01 -0600

    first and second commit. I forogt to commit the first one. SORRY!

---
+++ app.js
@@ -0,0 +1,31 @@
+
+/*
+
+
+How do we assign a value to a variable?     We assign it with an equal sign. let x = 0
+
+How do we change the value of a variable?   By re assigning the value with an equal sign. x = 3
+
+How do we assign an existing variable to a new variable?  Take the original variable and add it to a new one with an equal sign. let x = y 
+
+Remind me, what are declare, assign, and define? Declare and define are the same thing. Declare is allocating space for a new variable. Assign is assigning a variable a value. 
+
+What is pseudocoding and why should you do it?    Writing code out on paper, before actually starting the code.
+
+What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?   90% thinking about it and 10% actually coding.
+
+
+*/
+
+let firstVariable = "Hello World"
+
+firstVariable = 4
+
+let secondVariable = firstVariable
+
+secondVariable = "Hello"
+
+//4 is the value of the firstVariable
+
+let myName = "Johnathan"
+console.log("Hello, my name is " + myName)
