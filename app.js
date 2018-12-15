b38dad874351bf17e4449924eda0d5354d922a02
Jacob Werbin <jwerbin95@gmail.com>
2018-12-14 18:56:51 -0600

    Section 4: I transmogrify done

--- app.js
+++ app.js
@@ -199,3 +199,9 @@ function printLongestWord(strings)
 }
 console.log(printLongestWord(["sklajhfkjlas", "grdsgth", "gdhs", "fsdaf", "asdsdf"]))
 
+function transmogrify(num1, num2, num3)
+{
+	return (num1*num2)**num3
+}
+console.log(transmogrify(2, 5, 3))
+
