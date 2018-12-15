cad15915f05b12144075facdc041db0fdf26e21c
Johnathan <reddjohnathan@gmail.com>
2018-12-14 18:20:52 -0600

    Whats in Your Closet

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ if(rapper < 100){
 }else{
 	console.log('big number')
 }
-*/
+
 let number = 7
  if(number < 5){
  	console.log('little number')
@@ -161,7 +161,44 @@ let number = 7
  }else {
  	console.log('monkey')
  }
+*/
 
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+//console.log('Kristyn is rocking that ' + kristynsCloset[3] + ' today!' )
+kristynsCloset.splice(6, 0, 'raybans')
+kristynsCloset[5] = 'stained knit hat'
+//console.log('Thom is looking fierce in a ' + thomsCloset[0][0] + " , " + thomsCloset[1][2] + " and some " + thomsCloset[2][0])
+thomsCloset[1][2] = 'Footie Pajamas'
+console.log(thomsCloset)
 
 
 
