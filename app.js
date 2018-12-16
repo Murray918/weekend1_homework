6a128bae2b19cfb374fa2889e10d96b5f1cc50c1
pittpir <pittpir@gmail.com>
2018-12-16 12:51:57 -0600

    Part 3G (Arrays) -- Monkey in the Middle

--- app.js
+++ app.js
@@ -247,10 +247,53 @@ for (let i=0; i<bigSmall.length; ++i )
 }
 
 console.log(`******* #3.G) Monkey in the Middle *******` );
+bigSmall = [5,10,9,4,100,0,-1];
+for (let i=0; i<bigSmall.length; ++i )
+{
+	if (bigSmall[i] < 5 )
+	{
+		console.log(`Number is: ${bigSmall[i]}.  little number`);
+	}
+	else if (bigSmall[i] > 10 )
+	{
+		console.log(`Number is: ${bigSmall[i]}.  BIG number`);
 
+	}
+	else
+		console.log(`Number is: ${bigSmall[i]}.  MONKEY`);
+}
 
 
 console.log(`******* #3.H) What's in Your Closet? *******` );
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
 
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
 
 
