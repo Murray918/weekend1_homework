73d8f431eaff997e0bfcab24de958a24db74bed2
ohheyitslance <ohheyitslance@ohheyitslances-MacBook.local>
2018-12-16 12:55:25 -0600

    Completed Drivers Ed

--- app.js
+++ app.js
@@ -44,3 +44,12 @@ if (animal = "cow"){
 else {
 		console.log("Hey! You're not a cow!")
 	}
+
+// E.
+let driversAge = 16
+if (driversAge >= 16) {
+	console.log("Here are the keys.")
+}
+else {
+	console.log("Sorry, you're too young.")
+}
