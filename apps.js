ddfe6bf9fbb190631c9ad93e0579564c45515115
Sithiz <Joshklaas1@gmail.com>
2018-12-16 16:53:01 -0600

    Section A commplete

---
+++ apps.js
@@ -0,0 +1,20 @@
+//A 
+/*1. we assign a value of a variable using the = asignment operator.
+2. we reasign the value using = operator
+3. let a new variable and set it equal to your first varible 
+4. declare is to alociate space on memory for a varible, we then use the assignmet operator = to assign our new varible a value. 
+
+
+
+
+
+
+6. we should spend 75-90% planning and 25-10% coding.
+
+*/
+
+let animal = 'cow'
+
+if(animal === 'cow'){
+	console.log('moooo')
+}
