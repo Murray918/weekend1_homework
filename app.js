1de432cddca1f41cb2c3b00105979834f8b43779
Jamal Amar <JamalAmar@Jamals-MacBook-Air.local>
2018-12-16 10:57:54 -0600

    Give me Five solved

--- app.js
+++ app.js
@@ -91,9 +91,20 @@ for(let i = 12; i<4000; i++) {
 }
 */
 
-
+/*
 for(let i = 1; i<=100; i++) {
 	if (i%2===0) {
 		console.log(i, "<-- is an even number.")
+	}
+}
+*/
+
+
+for (let i = 0; i<=100; i++) {
+	if(i%5===0) {
+		console.log("I found a", i, "High five!")
+	}
+	else if (i%3===0) {
+		console.log("I found a", i, "Three is a crowd")
 	}
 }
