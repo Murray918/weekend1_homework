cc2349d94268db6f71e688c33704886aeb07e9b1
Jamal Amar <JamalAmar@Jamals-MacBook-Air.local>
2018-12-16 09:30:42 -0600

    Q+A answered

---
+++ app.js
@@ -0,0 +1,18 @@
+
+//1. How do we assign a value to a variable?
+//A. You declare the Variable and assing a value with "Equals (=)" (Let x = 10)	 
+
+//2. How do we change the value of a variable?
+//A. you just re-assing a value (x = 5)
+
+//3. How do we assign an existing variable to a new variable?
+//A. by assigning the existing variable to the new one (Let y = x)
+
+//4. Remind me, what are declare, assign, and define?
+//A. to declare is to create the variable, assing is giving the variable a value, and 
+
+//5. What is pseudocoding and why should you do it?
+//A. pseudocoding mean writting your code in you own words before you actually start coding. It helps you visualize and plan how are you going to code.
+
+//6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+//A. 75 -90% planning and 10-25% typing code.
