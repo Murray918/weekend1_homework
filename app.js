c19b81a26a1d1a4eca64a23c781edfda77bb5d8a
Julius Camarillo <julcamarillo17@gmail.com>
2018-12-16 10:27:24 -0600

    thru 2, b2.

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
-//b
-let firstVariable = "Hello World"
-firstVariable = 6
-console.log(firstVariable)
-let secondVariable = firstVariable
-secondVariable = "yooo"
-console.log(firstVariable)
-let yourName = "Jules Camarillo"
-console.log("Hello, my name is " + yourName +".")
-//c
-const a = 4;
-  const b = 53;
-  const c = 57;
-  const d = 16;
-  const e = 'Kevin';
+// //b
+// let firstVariable = "Hello World"
+// firstVariable = 6
+// console.log(firstVariable)
+// let secondVariable = firstVariable
+// secondVariable = "yooo"
+// console.log(firstVariable)
+// let yourName = "Jules Camarillo"
+// console.log("Hello, my name is " + yourName +".")
+// //c
+// const a = 4;
+//   const b = 53;
+//   const c = 57;
+//   const d = 16;
+//   const e = 'Kevin';
 
-   console.log(a < b);
-   console.log(c > d);
-   console.log('Name' === 'Name');
-   console.log(true > false);
-   console.log(false && false && false && false && false && false || true);
-   console.log(false === false)
-   console.log(e === 'Kevin');
-   console.log(a < b < c);
-   console.log(a && a < d);
-   console.log(48 !== '48');
+//    console.log(a < b);
+//    console.log(c > d);
+//    console.log('Name' === 'Name');
+//    console.log(true > false);
+//    console.log(false && false && false && false && false && false || true);
+//    console.log(false === false)
+//    console.log(e === 'Kevin');
+//    console.log(a < b < c);
+//    console.log(a && a < d);
+//    console.log(48 !== '48');
 
-//d
-let animal = "cow"
-if (animal === "cow"){
-	console.log("moooo")
-} else {console.log("Hey! You're not a cow!")}
+// //d
+// let animal = "cow"
+// if (animal === "cow"){
+// 	console.log("moooo")
+// } else {console.log("Hey! You're not a cow!")}
+
+// //e
+
+// let age = x
+// for ( let i = 10 ; i <25 ; i++){
+// 	if ( i < )
+// }
+
+//||
+//a.
+
+let x = 0
+while (x < 11){
+	console.log(x)
+	x++
+}
+//11 b
+let y = 10
+while (y < 401){
+	console.log(y)
+	y++
+}
