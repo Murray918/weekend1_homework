2299c0769f99f621dd8bb118f1508349277e7ff2
Sithiz <Joshklaas1@gmail.com>
2018-12-16 17:02:58 -0600

    Section B completed

--- apps.js
+++ apps.js
@@ -3,18 +3,24 @@
 2. we reasign the value using = operator
 3. let a new variable and set it equal to your first varible 
 4. declare is to alociate space on memory for a varible, we then use the assignmet operator = to assign our new varible a value. 
-
-
-
-
-
-
+5.pseudocoding is the process of writting out your plan for coding a oriject on paper, this allows you to plan ahead for problems that might arrive in the immediate future.
+Doing this also alows you to go into a project with a plan so your not blindly coding.
 6. we should spend 75-90% planning and 25-10% coding.
-
 */
+/*
+//B
+ let firstVariable  = 'Hello World'
 
-let animal = 'cow'
+ firstVariable = 67
 
-if(animal === 'cow'){
-	console.log('moooo')
-}
+ let secoundVariable = firstVariable
+
+secoundVariable = 'Goodbye world'
+
+// the value of first variable is the number 67
+
+//2B
+ let yourName = 'Joshua'
+
+ let myName = 'Hello, my name is ' + yourName
+/*
