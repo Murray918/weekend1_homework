029934b8cac94c2e247c1c7cf384bf89b99b033e
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-16 22:33:55 -0600

    answered Part 2 Section D

--- app.js
+++ app.js
@@ -165,3 +165,20 @@
 // }
 
 
+// // D. Savings account
+
+// // Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+// // Check your work! Your banck_account should have $55 in it.
+// let bank_account = 0;
+// for (let i = 1; i <= 10; i++) {
+// bank_account += i;
+// }
+// console.log(bank_account);
+
+// // You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// // Check your work! Your banck_account should have $10,100 in it.
+// bank_account = 0
+// for (let j = 1; j <= 100; j++) {
+// bank_account += j*2;
+// }
+// console.log(bank_account);
