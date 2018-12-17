02e04a3cfb806092b1da0aefa0d8583d7e87ec97
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-16 23:55:39 -0600

    answered Part 3 Section H

--- app.js
+++ app.js
@@ -332,7 +332,64 @@
 // }
 
 
+// // H. What's in Your Closet?
 
+// // Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
+
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "GA hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
+
+// // // Thom's closet is more complicated. Check out this nested data structure!!
+
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
+
+// // What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!")
+
+// // Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+// kristynsCloset[6] = "raybans"
+
+// // Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+// kristynsCloset[5] = "stained knit hat"
+// // Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+// thomsCloset[0][0];
+
+// // In the same way, access one item from Thom's pants array.
+// thomsCloset[1][0];
+
+// // Access one item from Thom's accessories array.
+// thomsCloset[2][1];
+
+// // Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// console.log(`Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][0]}, and ${thomsCloset[2][1]}!`);
+
+// // Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+// thomsCloset[1][2] = "Footie Pajamas";
 
 
 
