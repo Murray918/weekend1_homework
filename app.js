ae06fcad28455bf0842f0c8f6bb23d6f5d2d04a9
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-16 23:44:42 -0600

    answered Part 3 Section G

--- app.js
+++ app.js
@@ -315,12 +315,23 @@
 // }
 
 
-// G. Monkey in the Middle
+// // G. Monkey in the Middle
 
-// Write an if ... else if ... else statement:
-// console.log() little number if the number entered is less than 5.
-// If the number entered is more than 10, log big number.
-// Otherwise, log "monkey". 
+// // Write an if ... else if ... else statement:
+// // console.log() little number if the number entered is less than 5.
+// // If the number entered is more than 10, log big number.
+// // Otherwise, log "monkey". 
+
+// let x = 8;
+// if (x <5) {
+// 	console.log("little number");
+// } else if (x > 10) {
+// 	console.log("big number");
+// } else {
+// 	console.log("monkey");
+// }
+
+
 
 
 
