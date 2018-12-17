b6ef130174335b6339579abdc7dd23b9fdf2ac80
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-16 20:16:05 -0600

    answered Part I Section A

---
+++ app.js
@@ -0,0 +1,37 @@
+// Variables and Datatypes
+
+// // A. Q + A
+// How do we assign a value to a variable?
+// We use the '=' token to assign value to a declared variable.
+// ex. let a = 'assigned'
+
+// How do we change the value of a variable?
+// We change the value of a variable by reassigning the variable with a new statement.
+// ex. a = 'changed'
+
+// How do we assign an existing variable to a new variable?
+// If I understand the question correctly, we declare the new variable, and put the existing one after the '='.
+// ex. let b = a
+
+// Remind me, what are declare, assign, and define?
+// To declare a variable is to allocate memory for it. To define it is to assign the declared variable a value.
+// ex. let c (declare variable c) = (assign) 'cupcake' (definition) > (= 'cupcake') is where we define variable c.
+
+// What is pseudocoding and why should you do it?
+// Pseudocoding is the act of writing out code in plain English before going about trying to actually write it in
+// JavaScript (or another language). We should do it because it helps us to lay out our objectives and plan of attack in
+// more natural language before we go about trying to take the steps and piece by piece use them to create JavaScript code.
+// I think in some way it could be like writing an outline of a paper before you even write the first draft.
+
+// What percentage of time should be spent thinking about how you're going to solve a problem
+// vs actually typing in code to solve it?
+// We should spend the vast majority of our time thinking about how we're going to solve a problem
+// before we go about actually writing the code. I still struggle with this concept because I feel like just going in and
+// writing some JavaScript helps to break the ice and get me thinking. Even if I have to scrap everything and start all over,
+// it helps me to get through the process of solving the problem.
+
+
+
+
+
+
