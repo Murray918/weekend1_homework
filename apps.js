e3d9713f6672e97615f824adcd246fb51df4c80b
Sithiz <Joshklaas1@gmail.com>
2018-12-16 19:04:43 -0600

    Section 2E completed

--- apps.js
+++ apps.js
@@ -100,7 +100,7 @@ for(let i = 0; i<=100; i++){
 }
 */
 //2D
-
+/*
 let bank_account = 0
 for(let i = 0; i<=10; i++){
 	bank_account += i
@@ -110,11 +110,16 @@ for(let i = 0; i<=100;i++){
 	bank_account += i*2
 }
 console.log(bank_account)
-
-
-
-
-
+*/
+//2E
+/*
+let sum = 0
+for( let i = 0; i<= 1000; i++){
+	if(i %3 === 0 || i % 5 === 0){
+		sum += i
+	}
+}
+*/
 
 
 
