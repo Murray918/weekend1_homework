3217965d211aa555216d4c88841909ff0b254a32
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-17 02:19:20 -0600

    answered Objects part F

--- app.js
+++ app.js
@@ -716,6 +716,18 @@ user.friend.purchased.push("A latte");
 console.log(user.friend.purchased[1]);
 
 
+// F. Loops
+// Write a for loop that iterates over the User's purchased array
+// (NOT the friend's purchased array), and prints each element to the console.
+// Write a for loop that iterates over the Friend's purchased array, and prints each element to the console.
+
+for (let i = 0; i < user.purchased.length; i++) {
+	console.log(user.purchased[i]);
+}
+
+for (let i = 0; i < user.friend.purchased.length; i++) {
+	console.log(user.friend.purchased[i]);
+}
 
 
 
